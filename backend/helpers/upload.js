0c0a8de23ffcaeedd7775197f7b4851a45e8d7a0
Brunoake <brunopachecogre@gmail.com>
2024-08-08 13:40:56 -0300

    .

--- backend/helpers/upload.js
+++ backend/helpers/upload.js
@@ -4,15 +4,15 @@ const path = require("path")
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, path.join(__dirname, "../public/image/"))
+        cb(null, path.join(__dirname, "../public/images"))
     },
     filename: function(req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname))
     }
-})
+}) 
 
 const filterFilter = (req, file,  cb) => {
-    if(file.mimetype === "image/jpg" || file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
+    if(file.mimetype === "image/jpg" || file.mimetype === "image/png") {
         cb(null, true);
     }else{
         cb(null, false)
